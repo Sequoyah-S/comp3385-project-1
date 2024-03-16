cb6db6fccc47c02d2f1d474a0a1aa5a5038cd089
Sequoyah <quoyahhs@hotmail.com>
2024-03-16 17:26:28 -0400

    added database/migrations/2024_03_16_205427_create_properties_table.php

---
+++ database/migrations/2024_03_16_205427_create_properties_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('properties', function (Blueprint $table) {
+            $table->id();
+            $table->string('property_title');
+            $table->text('description');
+            $table->string('location');
+            $table->integer('no_of_rooms');
+            $table->decimal('no_of_bathrooms');
+            $table->string('property_type');
+            $table->string('photo')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('properties');
+    }
+};
